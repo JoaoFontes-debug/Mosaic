9416bac324ad1f6183cb21763728729f740e8593
João Fontes <j.henry.kings@gmail.com>
2025-06-02 23:08:50 -0300

    refatorando classes

--- Mosaic/src/main/java/com/joaofontes/mosaic/Mosaic.java
+++ Mosaic/src/main/java/com/joaofontes/mosaic/Mosaic.java
@@ -1,45 +1,19 @@
 
 package com.joaofontes.mosaic;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import javax.swing.JOptionPane;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import view.Moldura;
-/**
- *
- * @author JoãoFontes
- */
+import view.JanelaPrincipal;
+
+
 public class Mosaic {
 
     public static void main(String[] args) {
-       
-        
-          /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new Moldura().setVisible(true);
+    
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            JanelaPrincipal.getInstance().setVisible(true);
         });
         
         
-        
-        try {
-            Connection con;
-            Statement st;
-            
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbTesteMosaic", "root", "admin");
-            st = con.createStatement();
-            
-            st.executeUpdate("insert into tbimagens(url, descricao, nome_peca) values('google.com', 'teste da conexao', 'porca') ");
-             JOptionPane.showMessageDialog(null, "dados inseridos com sucesso!!!!");
-        } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "O driver não está na biblioteca");
-            
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro na conexao ou operações no banco de dados");
-            
-        }
-        
     }
+    
+    
 }
